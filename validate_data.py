8db2534adfc5fb9a347c92b78050c2bfc1cec185
maxmillan45 <maxmillannjue@gmail.com>
2026-05-28 20:45:31 +0300

    Add data validation helper script

---
+++ validate_data.py
@@ -0,0 +1,5 @@
+def validate_article_data():
+    """Validate that all required fields are present in article data"""
+    required_fields = ['article_number', 'title', 'full_text']
+    return required_fields
+
