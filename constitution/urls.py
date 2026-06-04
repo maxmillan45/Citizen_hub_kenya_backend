84856696bc550923e76248ea3cb702427a0c96e0
maxmillan45 <maxmillannjue@gmail.com>
2026-06-04 16:31:55 +0300

    Fix constitution URLs - add test-articles route

--- constitution/urls.py
+++ constitution/urls.py
@@ -1,14 +1,9 @@
 from django.urls import path
-from .test_only import TestOnlyView
-from .views import ArticleSearchView, ArticleDetailView
+from . import views
+from .test_view import test_articles
 
 urlpatterns = [
-    path('test-only/', TestOnlyView.as_view(), name='test-only'),
-    path('search/', ArticleSearchView.as_view(), name='article-search'),
-    path('article/<str:article_number>/', ArticleDetailView.as_view(), name='article-detail'),
-]
-from .health_check import URLListView
-
-urlpatterns += [
-    path('urls/', URLListView.as_view(), name='url-list'),
+    path('search/', views.ArticleSearchView.as_view(), name='article-search'),
+    path('article/<str:article_number>/', views.ArticleDetailView.as_view(), name='article-detail'),
+    path('test-articles/', test_articles, name='test-articles'),
 ]
